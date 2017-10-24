0fb37b52b731001a31652b85498e92a565ec9ea9
huseyin <hkoma7@gmail.com>
2017-10-24 21:21:29 +0300

    updates

--- main.py
+++ main.py
@@ -17,17 +17,24 @@ api = tweepy.API(auth)
 def tweet_to_json(tweet):
     tweetcolls = {
         'created_at': tweet.created_at,
+        'text': tweet.text,
+        'language': tweet.lang,
     }
     return tweetcolls
+
+
+def algorithm_for_search(tweet):
+    something = tweet
+
 
 search_results = api.search(q="grip", count=100)
 client = MongoClient('172.17.0.1', 27017)
 db = client.tweets
 for tweet in search_results:
-    db.tweetcolls.insert(tweet_to_json(tweet))
+    if tweet.lang == 'tr':
+        db.tweetcolls.insert(tweet_to_json(tweet))
 '''
 def find_disease(){
-
 }
 '''
 
