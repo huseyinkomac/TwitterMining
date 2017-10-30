8eb1193cb68cdb2411f09f55ba5793798d17f37f
huseyin <hkoma7@gmail.com>
2017-10-30 11:13:24 +0300

    changes 2

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import tweepy
 from pymongo import MongoClient
 from tweepy import OAuthHandler
+import re
 
 
 def tweet_to_json(tweet):
@@ -10,9 +11,51 @@ def tweet_to_json(tweet):
         'text': tweet.text,
         'language': tweet.lang,
         'location': tweet.geo,
-        'coordinates': tweet.coordinates
+        'metadata': tweet.metadata,
+        'coordinates': tweet.coordinates,
+        'source_url': tweet.source_url,
+        'source': tweet.source,
+        'contributors': tweet.contributors,
+        'retweeted': tweet.retweeted,
+        'entities': tweet.entities,
+        'id':tweet.id
     }
     return tweetcolls
+
+
+emoticons_str = r"""
+    (?:
+        [:=;]
+        [oO\-]? 
+        [D\)\]\(\]/\\OpP]
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',
+    r'(?:@[\w_]+)',
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',
+    r"(?:[a-z][a-z'\-_]+[a-z])",
+    r'(?:[\w_]+)',
+    r'(?:\S)'
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
 
 
 if __name__ == '__main__':
@@ -24,11 +67,11 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
 
-    search_results = api.search(q="flu", rpp=100)
+    search_results = api.search(q="grip", rpp=100)
 
     client = MongoClient('172.17.0.1', 27017)
     db = client.tweets
     for tweet in search_results:
         print (dir(tweet))
-        if tweet.lang == 'en':
+        if tweet.lang == 'tr':
             db.tweetcolls.insert(tweet_to_json(tweet))
